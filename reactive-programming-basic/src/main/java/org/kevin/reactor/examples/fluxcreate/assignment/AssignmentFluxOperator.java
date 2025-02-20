056a96e8f83b670cb184172b68862e237b6d06d8
Kevin T <tsaguedongmo@mail.com>
2025-02-20 15:22:12 +0100

    EIN-50: Final Assesement

--- reactive-programming-basic/src/main/java/org/kevin/reactor/examples/fluxcreate/assignment/AssignmentFluxOperator.java
+++ reactive-programming-basic/src/main/java/org/kevin/reactor/examples/fluxcreate/assignment/AssignmentFluxOperator.java
@@ -8,10 +8,9 @@ public class AssignmentFluxOperator {
 
         //only product-names from 1..4 are available
         ExternalServiceClient client = new ExternalServiceClient();
-//        for (int i=1; i<=5; i++){
-//            client.getProductName(i).subscribe(Util.subscriber());
-//        }
-        client.getProductName(3).subscribe(Util.subscriber());
+        for (int i=1; i<=5; i++){
+            client.getProductName(i).subscribe(Util.subscriber());
+        }
 
         Util.sleepSeconds(3);
     }
